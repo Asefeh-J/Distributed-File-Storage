6d9ec635042ab42ee9cde9c4bbcb1777f1d4e12b
Asefeh-J <aasefeh@gmail.com>
2025-02-18 14:48:22 +0330

    update file.go

--- shared/models/file.go
+++ shared/models/file.go
@@ -1,8 +1,10 @@
 package models
 
+import "gorm.io/gorm"
+
 type File struct {
-	ID       string            `json:"id"`
-	Name     string            `json:"name"`
-	Size     int64             `json:"size"`
-	Metadata map[string]string `json:"metadata"`
+	gorm.Model        // Adds ID, CreatedAt, UpdatedAt, DeletedAt fields
+	Name       string `json:"name"`
+	Size       int64  `json:"size"`
+	Metadata   string `json:"metadata"` // JSON string to store metadata
 }
